41a3ab8c4f6eaf70a68c6fa866e8a570e9c8dc59
ParTee10 <marvinmenaling143@gmail.com>
2022-03-29 12:55:26 +0800

    first commit

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/member-ordering */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor() {}
+  option = {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    loop: false,
+    spaceBetween: 10,
+  };
+}
